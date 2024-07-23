d0c7f79ecc8ad67b2e8f869636f4be9159c00576
Vincent Escoffier <alainvincentesc@gmail.com>
2024-07-23 15:24:18 +0200

    refining prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -9,6 +9,9 @@ export async function POST(req: Request) {
 
   const result = await streamText({
     model: openai("gpt-4o"),
+    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
     messages: convertToCoreMessages(messages),
   });
 
